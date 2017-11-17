41022d72b4ccaed5bc73477d3440fbf7ae4c558d
Inna <designbyinna@gmail.com>
2017-11-17 14:44:31 -0700

    final

--- js/index.js
+++ js/index.js
@@ -31,15 +31,15 @@ $("document").ready(function(){
    })
 
 
-  $(document).click(function(e) {
-    //if the menu is visible
-    if($(".scrolled-menu").show()) {
-        //if the click is outside of menu
-        if($(e.target).closest(".scrolled-menu").length === 0){
-            $('.drop-down-menu').hide();
-        }
-    }
-});
+//   $(document).click(function(e) {
+//     //if the menu is visible
+//     if($(".scrolled-menu").show()) {
+//         //if the click is outside of menu
+//         if($(e.target).closest(".scrolled-menu").length === 0){
+//             $('.drop-down-menu').hide();
+//         }
+//     }
+// });
 
 
 });
