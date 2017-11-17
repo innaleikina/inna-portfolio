6a9c7f57048ce363a1708c1b22edf73914db09ef
Inna <designbyinna@gmail.com>
2017-11-17 14:39:12 -0700

    images-menu

--- js/index.js
+++ js/index.js
@@ -17,25 +17,29 @@ $("document").ready(function(){
            $(".scrolled-menu").css("display","none");
 
      }
-         
+
   });
-  
+
   $(".drop-down-menu").hide();
-  
+
   $(".fa-bars").on("click", function(){
      $(".drop-down-menu").toggle();
    });
-  
-  
+
+   $(".drop-down-menu").on("click", function(){
+     $(".drop-down-menu").hide();
+   })
+
+
   $(document).click(function(e) {
     //if the menu is visible
     if($(".scrolled-menu").show()) {
         //if the click is outside of menu
         if($(e.target).closest(".scrolled-menu").length === 0){
             $('.drop-down-menu').hide();
-        } 
-    }       
+        }
+    }
 });
 
-   
+
 });
