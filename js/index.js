42856a4b95367dc49b31ea80316e35373a9f8807
Inna <designbyinna@gmail.com>
2017-12-12 22:07:23 -0700

    initial commit

--- js/index.js
+++ js/index.js
@@ -2,46 +2,9 @@
 
 $("document").ready(function(){
 
-  $(window).scroll(function() {
-
-  var scroll = $(window).scrollTop();
-
-//navigation appers when user scrolls
-     if (scroll >= 150) {
-        $(function () {
-            $(".all-small-boxes").hide();
-    $(".scrolled-menu").css("display","block");
-    $(".scrolled-menu").css("position","fixed");
-        })
-     } else {
-         $(".all-small-boxes").show();
-           $(".scrolled-menu").css("display","none");
-
-     }
-
-  });
-
-//when user clicks the drop down menu it automatically closes, after taking the user to the requested page
-  $(".drop-down-menu").hide();
-
-  $(".fa-bars").on("click", function(){
-     $(".drop-down-menu").toggle();
-   });
-
-   $(".drop-down-menu").on("click", function(){
-     $(".drop-down-menu").hide();
-   })
-
-
-//   $(document).click(function(e) {
-//     //if the menu is visible
-//     if($(".scrolled-menu").show()) {
-//         //if the click is outside of menu
-//         if($(e.target).closest(".scrolled-menu").length === 0){
-//             $('.drop-down-menu').hide();
-//         }
-//     }
-// });
+$(".about").on("click", function(){
+alert("About Inna");
+})
 
 
 });
